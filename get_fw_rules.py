cb124c10712e01776632198a32e7dad95d4242ce
Matt Watkins <mattyboy.watkins@gmail.com>
2021-07-09 23:08:44 +0100

    added get_fw_rules

---
+++ get_fw_rules.py
@@ -0,0 +1,14 @@
+import meraki
+import variables
+import pprint
+
+API_KEY = variables.KEY
+NETWORK_MX12 = "L_676102894059004221"
+NETWORK_MX34 = "L_676102894059004220"
+dashboard = meraki.DashboardAPI(API_KEY)
+
+response = dashboard.appliance.getNetworkApplianceFirewallL3FirewallRules(
+    NETWORK_MX12
+)
+
+pprint.pprint(response)
