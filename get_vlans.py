2c653d0229ae29d775b2d87f4910b13813ce81cf
Matt Watkins <mattyboy.watkins@gmail.com>
2021-07-13 15:49:27 +0100

    Looped through rather than just printing the JSON

--- get_vlans.py
+++ get_vlans.py
@@ -1,5 +1,4 @@
 import meraki
-import pprint
 import variables
 
 API_KEY = variables.KEY
@@ -9,4 +8,11 @@ BASE_URL = "https://api.meraki.com/api/v1"
 dashboard = meraki.DashboardAPI(API_KEY, suppress_logging=True)
 
 response = dashboard.appliance.getNetworkApplianceVlans(NETWORK_MX34)
-pprint.pprint(response)
+
+for vlan in response:
+    print('-----------------------------------------------------------------------------------------------')
+    print(f'Name        : {vlan["name"]}')
+    print(f'Subnet      : {vlan["subnet"]}')
+    print(f'Gateway     : {vlan["applianceIp"]}')
+    print(f'DHCP        : {vlan["dhcpHandling"]}, DNS: {vlan["dnsNameservers"]}')
+    print(f'Reserved IPs: {vlan["reservedIpRanges"]}')
