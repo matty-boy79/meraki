18fd7298bb021d9da3c6e63326a42d07ac417367
Matt Watkins <mattyboy.watkins@gmail.com>
2021-07-13 15:28:42 +0100

    renamed scripts

--- get_fw_rules.py
+++ get_fw_rules.py
@@ -5,7 +5,7 @@ import pprint
 API_KEY = variables.KEY
 NETWORK_MX12 = "L_676102894059004221"
 NETWORK_MX34 = "L_676102894059004220"
-dashboard = meraki.DashboardAPI(API_KEY)
+dashboard = meraki.DashboardAPI(API_KEY, suppress_logging=True)
 
 response = dashboard.appliance.getNetworkApplianceFirewallL3FirewallRules(
     NETWORK_MX12
